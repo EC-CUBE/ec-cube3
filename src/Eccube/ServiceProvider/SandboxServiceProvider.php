188543a343e89581b05c0de94a821083aafd5e13
daichi_otani <daichi_otani@ec-cube.co.jp>
2023-09-20 17:09:15 +0900

    ホワイトリストの重複・不要箇所の削除

--- src/Eccube/ServiceProvider/SandboxServiceProvider.php
+++ src/Eccube/ServiceProvider/SandboxServiceProvider.php
@@ -23,10 +23,10 @@ class SandboxServiceProvider implements ServiceProviderInterface
 
             // ホワイトリストの設定
             $tags = array('block','extends','for','if','set','spaceless','verbatim','with','form_theme','stopwatch','trans','trans_default_domain');
-            $filters = array('abs','batch','capitalize','date','default','doctrine_format_sql','doctrine_prettify_sql','doctrine_pretty_query','doctrine_replace_query_parameters','escape','first','format','abbr_class','abbr_method','file_link','file_relative','format_args','format_args_as_text','humanize','json_encode','keys','last','length','lower','merge','replace','round','split','striptags','title','trim','no_image_product','date_format','price','no_image_product','date_format','price','ellipsis','time_ago');
+            $filters = array('abs','batch','capitalize','date','escape','default','doctrine_format_sql','doctrine_prettify_sql','doctrine_pretty_query','doctrine_replace_query_parameters','first','format','abbr_class','abbr_method','file_link','file_relative','format_args','format_args_as_text','humanize','json_encode','keys','last','length','lower','merge','replace','round','split','striptags','title','trim','no_image_product','date_format','price','ellipsis','time_ago');
             $methods = array();
             $properties = array();
-            $functions = array('cycle','max','min','random','range','min','random','range','template_from_string','absolute_url','asset','asset_version','csrf_token','form_parent','fragment_uri','impersonation_exit_path','impersonation_exit_url','is_granted','logout_path','logout_url','path','relative_path','t','url','calc_inc_tax','active_menus','csrf_token_for_anchor','url','path','is_object','get_product');
+            $functions = array('cycle','max','min','random','range','template_from_string','absolute_url','asset','asset_version','csrf_token','form_parent','fragment_uri','impersonation_exit_path','impersonation_exit_url','is_granted','logout_path','logout_url','relative_path','t','calc_inc_tax','active_menus','csrf_token_for_anchor','url','path','is_object','get_product');
 
             $policy = new \Twig\Sandbox\SecurityPolicy($tags, $filters, $methods, $properties, $functions);
             $sandbox = new \Twig\Extension\SandboxExtension($policy);
@@ -40,6 +40,5 @@ class SandboxServiceProvider implements ServiceProviderInterface
 
     public function boot(Application $app)
     {
-        // TODO: Implement boot() method.
     }
 }
